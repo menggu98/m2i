9d217b693430d8b9a13bec72c73de0eefd0b4875
menggu98 <myengkyun@naver.com>
2023-06-28 09:59:29 +0900

    new

---
+++ 계산기/계산기/Form1.Designer.vb
@@ -0,0 +1,328 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Bdelete = New System.Windows.Forms.Button()
+        Me.Bclear = New System.Windows.Forms.Button()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.Bplusminus = New System.Windows.Forms.Button()
+        Me.B0 = New System.Windows.Forms.Button()
+        Me.Bresult = New System.Windows.Forms.Button()
+        Me.Bmulti = New System.Windows.Forms.Button()
+        Me.Bminus = New System.Windows.Forms.Button()
+        Me.Bslash = New System.Windows.Forms.Button()
+        Me.Bplus = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Bdot = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Bdelete
+        '
+        Me.Bdelete.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Bdelete.Location = New System.Drawing.Point(50, 185)
+        Me.Bdelete.Name = "Bdelete"
+        Me.Bdelete.Size = New System.Drawing.Size(76, 58)
+        Me.Bdelete.TabIndex = 0
+        Me.Bdelete.Text = "지우기"
+        Me.Bdelete.UseVisualStyleBackColor = True
+        '
+        'Bclear
+        '
+        Me.Bclear.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Bclear.Location = New System.Drawing.Point(132, 185)
+        Me.Bclear.Name = "Bclear"
+        Me.Bclear.Size = New System.Drawing.Size(76, 58)
+        Me.Bclear.TabIndex = 1
+        Me.Bclear.Text = "초기화"
+        Me.Bclear.UseVisualStyleBackColor = True
+        '
+        'B1
+        '
+        Me.B1.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B1.Location = New System.Drawing.Point(50, 249)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(76, 58)
+        Me.B1.TabIndex = 3
+        Me.B1.Text = "1"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'B2
+        '
+        Me.B2.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B2.Location = New System.Drawing.Point(132, 249)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(76, 58)
+        Me.B2.TabIndex = 4
+        Me.B2.Text = "2"
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'B3
+        '
+        Me.B3.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B3.Location = New System.Drawing.Point(214, 249)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(76, 58)
+        Me.B3.TabIndex = 9
+        Me.B3.Text = "3"
+        Me.B3.UseVisualStyleBackColor = True
+        '
+        'B4
+        '
+        Me.B4.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B4.Location = New System.Drawing.Point(50, 313)
+        Me.B4.Name = "B4"
+        Me.B4.Size = New System.Drawing.Size(76, 58)
+        Me.B4.TabIndex = 8
+        Me.B4.Text = "4"
+        Me.B4.UseVisualStyleBackColor = True
+        '
+        'B5
+        '
+        Me.B5.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B5.Location = New System.Drawing.Point(132, 313)
+        Me.B5.Name = "B5"
+        Me.B5.Size = New System.Drawing.Size(76, 58)
+        Me.B5.TabIndex = 7
+        Me.B5.Text = "5"
+        Me.B5.UseVisualStyleBackColor = True
+        '
+        'B6
+        '
+        Me.B6.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B6.Location = New System.Drawing.Point(214, 313)
+        Me.B6.Name = "B6"
+        Me.B6.Size = New System.Drawing.Size(76, 58)
+        Me.B6.TabIndex = 6
+        Me.B6.Text = "6"
+        Me.B6.UseVisualStyleBackColor = True
+        '
+        'B7
+        '
+        Me.B7.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B7.Location = New System.Drawing.Point(50, 377)
+        Me.B7.Name = "B7"
+        Me.B7.Size = New System.Drawing.Size(76, 58)
+        Me.B7.TabIndex = 5
+        Me.B7.Text = "7"
+        Me.B7.UseVisualStyleBackColor = True
+        '
+        'B8
+        '
+        Me.B8.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B8.Location = New System.Drawing.Point(132, 377)
+        Me.B8.Name = "B8"
+        Me.B8.Size = New System.Drawing.Size(76, 58)
+        Me.B8.TabIndex = 10
+        Me.B8.Text = "8"
+        Me.B8.UseVisualStyleBackColor = True
+        '
+        'B9
+        '
+        Me.B9.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B9.Location = New System.Drawing.Point(214, 377)
+        Me.B9.Name = "B9"
+        Me.B9.Size = New System.Drawing.Size(76, 58)
+        Me.B9.TabIndex = 11
+        Me.B9.Text = "9"
+        Me.B9.UseVisualStyleBackColor = True
+        '
+        'Bplusminus
+        '
+        Me.Bplusminus.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Bplusminus.Location = New System.Drawing.Point(50, 441)
+        Me.Bplusminus.Name = "Bplusminus"
+        Me.Bplusminus.Size = New System.Drawing.Size(76, 58)
+        Me.Bplusminus.TabIndex = 12
+        Me.Bplusminus.Text = "+/-"
+        Me.Bplusminus.UseVisualStyleBackColor = True
+        '
+        'B0
+        '
+        Me.B0.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.B0.Location = New System.Drawing.Point(132, 441)
+        Me.B0.Name = "B0"
+        Me.B0.Size = New System.Drawing.Size(76, 58)
+        Me.B0.TabIndex = 13
+        Me.B0.Text = "0"
+        Me.B0.UseVisualStyleBackColor = True
+        '
+        'Bresult
+        '
+        Me.Bresult.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Bresult.Location = New System.Drawing.Point(311, 441)
+        Me.Bresult.Name = "Bresult"
+        Me.Bresult.Size = New System.Drawing.Size(76, 58)
+        Me.Bresult.TabIndex = 18
+        Me.Bresult.Text = "="
+        Me.Bresult.UseVisualStyleBackColor = True
+        '
+        'Bmulti
+        '
+        Me.Bmulti.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Bmulti.Location = New System.Drawing.Point(311, 377)
+        Me.Bmulti.Name = "Bmulti"
+        Me.Bmulti.Size = New System.Drawing.Size(76, 58)
+        Me.Bmulti.TabIndex = 17
+        Me.Bmulti.Text = "*"
+        Me.Bmulti.UseVisualStyleBackColor = True
+        '
+        'Bminus
+        '
+        Me.Bminus.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Bminus.Location = New System.Drawing.Point(311, 249)
+        Me.Bminus.Name = "Bminus"
+        Me.Bminus.Size = New System.Drawing.Size(76, 58)
+        Me.Bminus.TabIndex = 16
+        Me.Bminus.Text = "-"
+        Me.Bminus.UseVisualStyleBackColor = True
+        '
+        'Bslash
+        '
+        Me.Bslash.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Bslash.Location = New System.Drawing.Point(311, 313)
+        Me.Bslash.Name = "Bslash"
+        Me.Bslash.Size = New System.Drawing.Size(76, 58)
+        Me.Bslash.TabIndex = 15
+        Me.Bslash.Text = "/"
+        Me.Bslash.UseVisualStyleBackColor = True
+        '
+        'Bplus
+        '
+        Me.Bplus.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Bplus.Location = New System.Drawing.Point(311, 185)
+        Me.Bplus.Name = "Bplus"
+        Me.Bplus.Size = New System.Drawing.Size(76, 58)
+        Me.Bplus.TabIndex = 14
+        Me.Bplus.Text = "+"
+        Me.Bplus.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextBox1.Font = New System.Drawing.Font("돋움", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(0, 56)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(337, 65)
+        Me.TextBox1.TabIndex = 19
+        '
+        'Bdot
+        '
+        Me.Bdot.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Bdot.Location = New System.Drawing.Point(214, 441)
+        Me.Bdot.Name = "Bdot"
+        Me.Bdot.Size = New System.Drawing.Size(76, 58)
+        Me.Bdot.TabIndex = 20
+        Me.Bdot.Text = "."
+        Me.Bdot.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextBox2.Font = New System.Drawing.Font("돋움", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(0, 16)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(337, 34)
+        Me.TextBox2.TabIndex = 21
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(50, 42)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(337, 137)
+        Me.Panel1.TabIndex = 22
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.Bresult
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.ClientSize = New System.Drawing.Size(457, 560)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Bdot)
+        Me.Controls.Add(Me.Bresult)
+        Me.Controls.Add(Me.Bmulti)
+        Me.Controls.Add(Me.Bminus)
+        Me.Controls.Add(Me.Bslash)
+        Me.Controls.Add(Me.Bplus)
+        Me.Controls.Add(Me.B0)
+        Me.Controls.Add(Me.Bplusminus)
+        Me.Controls.Add(Me.B9)
+        Me.Controls.Add(Me.B8)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B7)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.Bclear)
+        Me.Controls.Add(Me.Bdelete)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "계산기"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Bdelete As Button
+    Friend WithEvents Bclear As Button
+    Friend WithEvents B1 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents B4 As Button
+    Friend WithEvents B5 As Button
+    Friend WithEvents B6 As Button
+    Friend WithEvents B7 As Button
+    Friend WithEvents B8 As Button
+    Friend WithEvents B9 As Button
+    Friend WithEvents Bplusminus As Button
+    Friend WithEvents B0 As Button
+    Friend WithEvents Bresult As Button
+    Friend WithEvents Bmulti As Button
+    Friend WithEvents Bminus As Button
+    Friend WithEvents Bslash As Button
+    Friend WithEvents Bplus As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Bdot As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ 계산기/계산기/Form1.vb
@@ -0,0 +1,205 @@
+﻿Public Class Form1
+    Dim calcmode As Integer = 0 '계산형태 ex) 더하기 빼기 곱하기 나누기
+    ' 0 이면 계산실행을 안했다는 의미
+    ' 1 더하기
+    ' 2 빼기
+    ' 3 곱하기
+    ' 4 나누기
+    Dim prev As String = "" ' 이전 실행했던 값을 의미
+    Dim caldone As Boolean = False ' 계산완료 초기값 False
+    Dim clearlog As Boolean = False ' 계산 후 기록 삭제용
+    Private Sub B1_Click(sender As Object, e As EventArgs) Handles B1.Click, B2.Click, B3.Click,
+        B4.Click, B5.Click, B6.Click, B7.Click, B8.Click, B9.Click, B0.Click, Bdot.Click
+        addvalue(sender.Text)
+    End Sub
+
+    Private Sub Bresult_Click(sender As Object, e As EventArgs) Handles Bresult.Click
+        runcal(True)
+    End Sub
+
+    Private Sub Bplus_Click(sender As Object, e As EventArgs) Handles Bplus.Click, Bminus.Click,
+            Bmulti.Click, Bslash.Click
+        Select Case sender.Name
+            Case Bplus.Name
+                setcalcmode(1)
+            Case Bminus.Name
+                setcalcmode(2)
+            Case Bmulti.Name
+                setcalcmode(3)
+            Case Bslash.Name
+                setcalcmode(4)
+        End Select
+    End Sub
+    Private Sub addvalue(value As String)
+        If Not calcmode = 0 Then
+            If prev = "" Then
+                prev = TextBox1.Text
+                TextBox1.Text = ""
+
+            End If
+        End If
+        If caldone Then
+            TextBox1.Text = ""
+            caldone = False
+        End If
+        If clearlog Then
+            TextBox2.Text = ""
+            clearlog = False
+        End If
+
+        TextBox1.Text += value
+    End Sub
+
+
+    Private Sub Bclear_Click(sender As Object, e As EventArgs) Handles Bclear.Click
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Bdelete_Click(sender As Object, e As EventArgs) Handles Bdelete.Click
+        TextBox1.Text = "" '지우기 버튼 클릭 시
+    End Sub
+
+    Private Sub Bplusminus_Click(sender As Object, e As EventArgs) Handles Bplusminus.Click
+        If IsNumeric(TextBox1.Text) Then
+            TextBox1.Text = (Convert.ToDouble(TextBox1.Text) * -1).ToString
+            ' convert.todouble 메서드로 부동소수점까지 표현
+            ' -1을 곱하여 음수로 표기하도록
+            ' tostring 문자열형태로
+        End If
+    End Sub
+
+    Private Sub setcalcmode(mode As Integer)
+        calcmode = mode
+        Bplus.ForeColor = Color.FromArgb(186, 206, 227) ' 푸르스름한 색으로 표현
+        Bminus.ForeColor = Color.FromArgb(186, 206, 227)
+        Bmulti.ForeColor = Color.FromArgb(186, 206, 227)
+        Bslash.ForeColor = Color.FromArgb(186, 206, 227)
+
+        Select Case mode
+            Case 1
+                Bplus.ForeColor = Color.SkyBlue
+            Case 2
+                Bplus.ForeColor = Color.SkyBlue
+            Case 3
+                Bplus.ForeColor = Color.SkyBlue
+            Case 4
+                Bplus.ForeColor = Color.SkyBlue
+        End Select
+    End Sub
+
+    Private Function calculator(num1 As String, num2 As String)
+        Dim a As Double = Convert.ToDouble(num1) ' 변수 a 선언하고 num1을 실수로 변환하고 저장
+        Dim b As Double = Convert.ToDouble(num2)
+
+        Select Case calcmode
+            Case 1
+                Return a + b ' 함수기에 반환값을 설정
+            Case 2
+                Return a - b
+            Case 3
+                Return a * b
+            Case 4
+                Return a / b
+
+        End Select
+    End Function
+
+    Private Sub runcal(fin As Boolean)
+        If prev = "" Then Exit Sub ' 이전 값이 없다면 프로시저 종료
+
+        Try
+            If TextBox2.Text = "" Then update(prev, True)
+            update(TextBox1.Text, False)
+            TextBox1.Text = calculator(prev, TextBox1.Text)
+
+                If fin Then
+                    clearlog = True
+                    TextBox2.Text += " ="
+
+                End If
+
+         Catch ex As Exception
+            TextBox1.Text = "오류"
+        End Try
+        resetcal()
+    End Sub
+
+    Private Sub update(plusval As String, numone As Boolean)
+        'TextBox2.Text += prev + " "
+
+        If Not numone Then
+            Select Case calcmode
+                Case 1
+                    TextBox2.Text += " + "
+                Case 2
+                    TextBox2.Text += " - "
+                Case 3
+                    TextBox2.Text += " x "
+                Case 4
+                    TextBox2.Text += " ÷ "
+            End Select
+
+        End If
+        TextBox2.Text += plusval
+    End Sub
+
+    Private Sub resetcal()
+        setcalcmode(0)
+        prev = ""
+        caldone = True
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Bresult.Focus()
+
+        Select Case e.KeyCode
+            Case Keys.D1, Keys.NumPad1
+                addvalue("1")
+            Case Keys.D2, Keys.NumPad2
+                addvalue("2")
+            Case Keys.D3, Keys.NumPad3
+                addvalue("3")
+            Case Keys.D4, Keys.NumPad4
+                addvalue("4")
+            Case Keys.D5, Keys.NumPad5
+                addvalue("5")
+            Case Keys.D6, Keys.NumPad6
+                addvalue("6")
+            Case Keys.D7, Keys.NumPad7
+                addvalue("7")
+            Case Keys.D8, Keys.NumPad8
+                addvalue("8")
+            Case Keys.D9, Keys.NumPad9
+                addvalue("9")
+            Case Keys.D0, Keys.NumPad0
+                addvalue("0")
+            Case Keys.Decimal, Keys.OemPeriod
+                addvalue(".")
+            Case Keys.Enter
+                runcal(True)
+            Case Keys.Back
+                TextBox1.Text = Mid(TextBox1.Text, 1, TextBox1.Text.Length - 1)
+            Case Keys.Add, Keys.OemMinus, Keys.Subtract, Keys.Multiply, Keys.Divide
+                If Not calcmode = 0 And Not prev = "" Then runcal(False)
+
+                Select Case e.KeyCode
+                    Case Keys.Add
+                        setcalcmode(1)
+                    Case Keys.OemMinus, Keys.Subtract
+                        setcalcmode(2)
+                    Case Keys.Multiply
+                        setcalcmode(3)
+                    Case Keys.Divide
+                        setcalcmode(4)
+                End Select
+            Case Keys.Escape
+                resetcal()
+                TextBox1.Text = ""
+                TextBox2.Text = ""
+            Case Keys.Delete
+                TextBox1.Text = ""
+        End Select
+    End Sub
+End Class
